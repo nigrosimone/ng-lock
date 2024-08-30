555c0abbecfd8ebb747fbe13eaec856d4f68ecfd
Nigro Simone <nigro.simone@gmail.com>
2024-08-30 21:51:07 +0200

    docs: improve

--- projects/ng-lock/src/lib/ng-lock.directive.ts
+++ projects/ng-lock/src/lib/ng-lock.directive.ts
@@ -3,11 +3,54 @@ import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { NgLockFunction } from './ng-lock-types';
 import { ngLockHtmlElement, ngLockObservable, ngLockOption, ngUnLockHtmlElement } from './ng-lock.decorator';
 
+
+/**
+ * @ngModule NgLockModule
+ *
+ * @description
+ *
+ * The `ngLock` directive it's a Angular directive lock html element when a decorated method with `@ngLock` is running a task.
+ *
+ * @usageNotes
+ *
+ * ### Usage
+ *
+ * ```html
+ * <input [ngLock]="myMethod" /><button (click)="myMethod($event)">Send</button>
+ * ```
+ * ```ts
+ * @ngLock()
+ * myMethod(event: MouseEvent){
+ *  return new Promise(resolve => setTimeout(resolve, 5000));
+ * }
+ * ```
+ */
 @Directive({
     selector: '[ngLock]'
 })
 export class NgLockDirective implements OnInit {
 
+    /**
+     * @ngModule NgLockModule
+     *
+     * @description
+     *
+     * The `ngLock` directive it's a Angular directive lock html element when a decorated method with `@ngLock` is running a task.
+     *
+     * @usageNotes
+     *
+     * ### Usage
+     *
+     * ```html
+     * <input [ngLock]="myMethod" /><button (click)="myMethod($event)">Send</button>
+     * ```
+     * ```ts
+     * @ngLock()
+     * myMethod(event: MouseEvent){
+     *  return new Promise(resolve => setTimeout(resolve, 5000));
+     * }
+     * ```
+     */
     @Input({ required: true }) ngLock!: NgLockFunction;
 
     constructor(
