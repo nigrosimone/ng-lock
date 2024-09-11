f80620302f0a24e54a540469cf13243bb6a121b8
Nigro Simone <nigro.simone@gmail.com>
2024-09-11 06:33:18 +0200

    chore: better ts

--- projects/ng-lock/src/lib/ng-lock-rxjs.ts
+++ projects/ng-lock/src/lib/ng-lock-rxjs.ts
@@ -1,13 +1,13 @@
-import { Observable, tap } from "rxjs";
+import { MonoTypeOperatorFunction, Observable, tap } from "rxjs";
 import type { NgLockFunction } from "./ng-lock-types";
 import { ngUnlock } from "./ng-lock-utils";
 
 /**
  * Unlock the method when Observable changes
  * @param {NgLockFunction} methodToUnlock method to unlock
- * @returns {(source$: Observable<T>) => Observable<T>}
+ * @returns {MonoTypeOperatorFunction<T>}
  */
-export const ngLockChanges = <T>(methodToUnlock: NgLockFunction): (source$: Observable<T>) => Observable<T> => {
+export const ngLockChanges = <T>(methodToUnlock: NgLockFunction): MonoTypeOperatorFunction<T> => {
     return (source$: Observable<T>): Observable<T> => {
         return source$.pipe(tap(() => ngUnlock(methodToUnlock, 'Observable changes')));
     }
